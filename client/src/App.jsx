5c0c541916511c139d40fbbbf3e5e6b62f53dfd0
nottoezz <liam@birch.co.za>
2025-09-08 00:58:04 +0200

    Fix github pages 404 err

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
     // top-level routing
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/iSearch">
       <AuthProvider>
         <ToastProvider>
           <Navbar />
